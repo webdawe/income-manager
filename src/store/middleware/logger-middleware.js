a8cabf560451142f42062e6111fb52104dd203cc
Anil Paul <anilnpaul@gmail.com>
2025-03-04 13:17:30 +1100

    redux listner Middleware action matcher

--- src/store/middleware/logger-middleware.js
+++ src/store/middleware/logger-middleware.js
@@ -1,11 +1,14 @@
-const { createListenerMiddleware } = require("@reduxjs/toolkit");
+import { addExpenseAction, setIncomeAction } from "store/expense/expense-slice";
+
+const { createListenerMiddleware, isAnyOf } = require("@reduxjs/toolkit");
 
 export const loggerMiddleware = createListenerMiddleware();
 
 loggerMiddleware.startListening({
-  predicate: (action) => {
+  /*predicate: (action) => {
     return true;
-  },
+  },*/
+  matcher: isAnyOf(setIncomeAction, addExpenseAction),
   effect: async (action, listenerAPI) => {
     console.log("Action: ", action);
     console.log("New Store Value", listenerAPI.getState());
