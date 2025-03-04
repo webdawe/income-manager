40e0f68afc4521a426404575b945f15052b0337c
Anil Paul <anilnpaul@gmail.com>
2025-03-04 12:55:50 +1100

    redux store persistance

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { App } from "./App";
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { expenseSlice } from "./expense/expense-slice";
 import { persistStore, persistReducer } from "redux-persist";
-import { storage } from "redux-persist/lib/storage";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  EXPENSE: expenseSlice.reducer,
+});
+const persistConfig = {
+  key: "expensemanagerRoot",
+  storage: storage,
+};
+
+const persistedReducers = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
-  reducer: {
-    EXPENSE: expenseSlice.reducer,
-  },
+  reducer: persistedReducers,
 });
 
-export { store };
+const persistor = persistStore(store);
+export { store, persistor };
